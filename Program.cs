84cc83795478a60b1c9675c5a4ce249a4000e9b5
Marta Neris <78042453+Martha-boop@users.noreply.github.com>
2021-03-29 18:45:17 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+
+namespace CalculandoMediaIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal media = 0;
+            decimal[]  idade = new decimal[8]
+            
+            {
+                30, 42, 54, 32, 45, 65, 57,21
+            };
+            media = ( 30 + 42 + 54 + 32 + 45 + 65 + 57+ 21) / 8;
+            Console.WriteLine("A soma da Media é:");
+            Console.WriteLine(idade[1] + media);
+            
+               
+            Console.ReadLine();
+        }
+    }
+}
